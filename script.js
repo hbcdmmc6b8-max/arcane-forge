f1fbdd5f0d7d8a45dbfcb8627787ee39b0c512c1
hbcdmmc6b8-max <hbcdmmc6b8@privaterelay.appleid.com>
2026-09-19 09:16:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -826,7 +826,7 @@ Object.assign(
   {
     position: "fixed",
     left: "50%",
-    top: "125px",
+    top: "155px",
     transform: "translateX(-50%)",
     width: "min(520px, 72vw)",
     zIndex: "56",
